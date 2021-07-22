fef55322971760f51d6e45bcd9b65bfca9903e0d
Carl <carlparm@gmail.com>
2021-07-21 20:39:35 -0400

    postgreSQL setting update

--- wineapp/settings.py
+++ wineapp/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'wineapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'mydatabase',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wineapp',
+        'USER': 'postgres',
+        'PASSWORD': 'Lizzy123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
