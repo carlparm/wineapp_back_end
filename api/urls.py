f248258338bc95caeb3ee5bcf3bfa93963e80bb9
Carl <carlparm@gmail.com>
2021-08-02 15:25:51 -0400

    Class based API CRUD Functionality

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,20 @@
 
 from django.urls import path
-from .views import wine_list, wine_details
+
+# Class Based API
+from .views import WineList, WineDetails
+
+# Fucntion Based API
+# from .views import wine_list, wine_details
 
 urlpatterns = [
-    path('wines/', wine_list),
-    path('wines/<int:pk>/', wine_details),
+
+    # Class Based API
+    path('wines/', WineList.as_view()),
+    path('wines/<int:id>/', WineDetails.as_view())
+
+    # Function Based API
+    # path('wines/', wine_list),
+    # path('wines/<int:pk>/', wine_details),
+
 ]
--- api/views.py
+++ api/views.py
@@ -2,47 +2,94 @@ from django.shortcuts import render, HttpResponse
 from rest_framework import serializers
 from .models import Wine
 from .serializers import WineSerializer
-from django.http import JsonResponse
-from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import APIView
 
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
-def wine_list(request):
-    if request.method == 'GET':
+# Class Based API
+
+class WineList(APIView):
+
+    def get(self, request):
         wines = Wine.objects.all()
         serializer = WineSerializer(wines, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request):
         serializer = WineSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def wine_details(request, pk):
-    try:
-        wine = Wine.objects.get(pk=pk)
+class WineDetails(APIView):
 
-    except Wine.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, id):
+        try:
+            return Wine.objects.get(id=id)
 
-    if request.method == 'GET':
+        except Wine.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        wine = self.get_object(id)
         serializer = WineSerializer(wine)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, id):
+        wine = self.get_object(id)
         serializer = WineSerializer(wine, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'DELETE':
+
+    def delete(self, request, id):
+        wine = self.get_object(id)
         wine.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+# Function bases API View
+
+# @api_view(['GET', 'POST'])
+# def wine_list(request):
+#     if request.method == 'GET':
+#         wines = Wine.objects.all()
+#         serializer = WineSerializer(wines, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = WineSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def wine_details(request, pk):
+#     try:
+#         wine = Wine.objects.get(pk=pk)
+
+#     except Wine.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'GET':
+#         serializer = WineSerializer(wine)
+#         return Response(serializer.data)
+
+#     elif request.method == 'PUT':
+#         serializer = WineSerializer(wine, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     elif request.method == 'DELETE':
+#         wine.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
